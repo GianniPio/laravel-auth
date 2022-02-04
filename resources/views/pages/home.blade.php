c4f8575934df8e3aae4e7a4e9936c3df765ee5d6
GianniPio <giannipiopellegrino00@gmail.com>
2022-02-04 17:11:26 +0100

    form for login

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,4 +16,21 @@
     <input type="submit" value="Register">
     
     </form>
+
+    <hr class="border-white">
+
+    <h1>Login</h1>
+
+    <form>
+
+        
+        <label for="email">Email</label>
+        <input type="text" name="email"><br>
+        <label for="password">Password</label>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Login">
+        
+        </form>
+
+
 @endsection
