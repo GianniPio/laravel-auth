4981b665ba352ceb048fb1a72b2101340cd39379
GianniPio <giannipiopellegrino00@gmail.com>
2022-02-04 17:09:05 +0100

    form for register

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Hello World</h1>
+    <h1>Register</h1>
+
+    <form>
+
+    <label for="name">Name</label>
+    <input type="text" name="name"><br>
+    <label for="email">Email</label>
+    <input type="text" name="email"><br>
+    <label for="password">Password</label>
+    <input type="password" name="password"><br>
+    <label for="password_confirmation">Password Confirmed</label>
+    <input type="password" name="password_confirmation"><br>
+    <input type="submit" value="Register">
+    
+    </form>
 @endsection
